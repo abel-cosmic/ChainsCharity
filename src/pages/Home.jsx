0ba8c4f9bcb528afcacc48690e66af1728186d8f
Dagim <dagimcodes@gmail.com>
2023-08-14 14:46:18 +0300

    Navigation Bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import About from "./home/About";
 import Contacts from "./home/Contact";
 import EventsPage from "./home/EventsPage";
+import NavigationBar from "./home/NavigationBar";
 import OurAim from "./home/OurAim";
 import Services from "./home/Services";
 import Testimonial from "./home/Testimonial";
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <>
       <TopNavBar/>
+      <NavigationBar/>
       <OurAim />
       <About />
       <Services />
---
+++ src/pages/home/NavigationBar.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { FormGroup, FormControlLabel, Switch } from "@mui/material";
+
+const NavigationBar = () => {
+  return (
+    <nav className="flex justify-between mx-10">
+      <img
+        className="w-[10rem]"
+        src="src/assets/images/Logo.png"
+        alt="chains-logo"
+      />
+      <div className="flex gap-10 font-semibold items-center">
+        <Link>Home</Link>
+        <Link>About</Link>
+        <Link>Services</Link>
+        <Link>Events</Link>
+        <Link>Teams</Link>
+        <Link>Contact</Link>
+        <div className="flex items-center gap-3 ">
+          <p>English</p>
+          <div className="movetoggle flex flex-col items-center h-20">
+            <p>language</p>
+            <input type="checkbox" className="toggle-button" />
+          </div>
+          <p>አማርኛ</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
