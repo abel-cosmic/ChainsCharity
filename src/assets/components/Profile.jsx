629b68afdc340666dc481435d7de2efeac4ce0db
Dagim <dagimcodes@gmail.com>
2023-08-16 21:28:38 +0300

    Fixed a bug in profile component

--- src/assets/components/Profile.jsx
+++ src/assets/components/Profile.jsx
@@ -11,43 +11,43 @@ const Profile = ({ profile }) => {
   return (
     <div className="profileCard bg-neutral flex flex-col w-[20rem] hover:shadow-blue">
       <div style={divImageStyle} className="h-[20rem] ">
-        <div className="socla-media-link flex items-end gap-10 mb-2 self-center justify-center pb-10 h-[35rem] ">
+        <div className="socla-media-link flex items-end gap-5 mb-2 self-center justify-center pb-10 h-[20rem] ">
           <Link
             to={profile.socialMedia.twitter}
-            className="bg-primary bg-opacity-70 p-3 rounded-lg hover:bg-opacity-100"
+            className="bg-primary bg-opacity-70 p-2 rounded-lg hover:bg-opacity-100"
           >
             <img
-              className="w-[2rem]"
+              className="w-[1.65rem]"
               src="src/assets/images/social medias/twitter.svg"
               alt="twitter-icon"
             />
           </Link>
           <Link
             to={profile.socialMedia.instagram}
-            className="bg-primary bg-opacity-70 p-3 rounded-lg  hover:bg-opacity-100"
+            className="bg-primary bg-opacity-70 p-2 rounded-lg  hover:bg-opacity-100"
           >
             <img
-              className="w-[2rem]"
+              className="w-[1.65rem]"
               src="src/assets/images/social medias/instagram.svg"
               alt="instagram-icon"
             />
           </Link>
           <Link
             to={profile.socialMedia.facebook}
-            className="bg-primary bg-opacity-70 p-3 rounded-lg  hover:bg-opacity-100"
+            className="bg-primary bg-opacity-70 p-2 rounded-lg  hover:bg-opacity-100"
           >
             <img
-              className="w-[2rem]"
+              className="w-[1.65rem]"
               src="src/assets/images/social medias/facebook.svg"
               alt="facebook-icon"
             />
           </Link>
           <Link
             to={profile.socialMedia.linkedin}
-            className="bg-primary bg-opacity-70 p-3 rounded-lg  hover:bg-opacity-100"
+            className="bg-primary bg-opacity-70 p-2 rounded-lg  hover:bg-opacity-100"
           >
             <img
-              className="w-[2rem]"
+              className="w-[1.65rem]"
               src="src/assets/images/social medias/linkedin.svg"
               alt="linkedin-icon"
             />
