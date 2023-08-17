c91cfd739ae0f96e95c4f6a644f78016bfcf7bc5
Abel Shibabaw <abelshibabaw@heuc.edu.et>
2023-08-17 18:04:28 +0300

    finished our aim component to amharic

--- src/pages/home/OurAim.jsx
+++ src/pages/home/OurAim.jsx
@@ -6,36 +6,35 @@ import arms_blue from "./../../assets/images/icons/Layer_1.svg";
 import arms from "./../../assets/images/icons/Layer_1 (1).svg";
 import earth from "./../../assets/images/icons/SVGRepo_iconCarrier (1).svg";
 import earth_blue from "./../../assets/images/icons/SVGRepo_iconCarrier (2).svg";
+import { useTranslation } from "react-i18next";
 export default function OurAim() {
+  const { t } = useTranslation();
   const card_aim = [
     {
       id: 1,
       img2: recylce_blue,
       img1: recylce,
-      heading: "Recycling for a Brighter Tomorrow",
-      description:
-        "Our soap recycling factory promotes sustainability by collecting, recycling, and sanitizing discarded soap from hotels and hospitality partners, transforming it into a valuable resource for the less fortunate.",
+      heading: t("aim-heading1"),
+      description: t("aim-description1"),
     },
     {
       id: 2,
       img2: arms_blue,
       img1: arms,
-      heading: "Lending a Helping Hand",
-      description:
-        "At Chains Charity, we provide soap and hygiene essentials to those in need. By donating repurposed soaps to shelters, orphanages, and communities, we make a positive difference in countless lives.",
+      heading: t("aim-heading2"),
+      description: t("aim-description2"),
     },
     {
       id: 3,
       img2: earth_blue,
       img1: earth,
-      heading: "Join Our Cause",
-      description:
-        "Your support is what keeps us going! We invite all of you to be part of this incredible journey by contributing to our cause. Whether through donations, volunteering, or simply spreading the word, you can help us reach even more communities and touch more lives.",
+      heading: t("aim-heading3"),
+      description: t("aim-description3"),
     },
   ];
   return (
     <div className="flex flex-col items-center justify-center gap-20 py-10 max-md:gap-10 max-md:py-4">
-      <PlaceHolder text="OUR AIM" />
+      <PlaceHolder text={t("aim")} />
       <div className="flex flex-row flex-wrap items-center gap-10 justify-center">
         {card_aim.map((card) => (
           <CardAim
