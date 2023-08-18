faf4c72adf4962aa9233223361f93e5d10b5547b
Dagim <dagimcodes@gmail.com>
2023-08-18 19:05:03 +0300

    Got rid of banner class

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -4,7 +4,7 @@ import i18n from "../../i18n";
 const Banner = () => {
   const { t } = useTranslation();
   return (
-    <section className="banner bg-no-repeat ">
+    <section className="bg-banner bg-cover bg-no-repeat">
       <div className="flex flex-col gap-5 lg:gap-10 px-10 lg:px-40 py-36 md:pt-60 md:pb-40 text-white">
         <p className="text-2xl lg:text-5xl">{t("banner-welcome")}</p>
         <p className="text-lg lg:text-xl">{t("banner-text")}</p>
