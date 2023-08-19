5c6931583420fb0e4753f2921d2719e5c8672397
Abel Shibabaw <abelshibabaw@heuc.edu.et>
2023-08-19 21:27:49 +0300

    animated backtotopbtn

--- src/assets/components/BackToTopBtn.jsx
+++ src/assets/components/BackToTopBtn.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import arrow from "./../images/icons/mdi_arrow-up.svg";
+import { motion } from "framer-motion";
 
 const BackToTopBtn = () => {
   const [backToTopBtn, setBackToTopBtn] = useState(false);
@@ -22,12 +23,15 @@ const BackToTopBtn = () => {
   return (
     <div>
       {backToTopBtn && (
-        <button
+        <motion.button
           onClick={scrollUp}
+          whileHover={{ scale: 1.2 }}
+          initial={{ scaleX: 0, scaleY: 0 }}
+          animate={{ scaleX: 1, scaleY: 1 }}
           className="flex flex-row items-center justify-center py-4 px-4 rounded bg-primary bg-opacity-90 hover:bg-opacity-100 fixed bottom-6 right-10 "
         >
           <img src={arrow} alt="arrow" className="w-6" />
-        </button>
+        </motion.button>
       )}
     </div>
   );
