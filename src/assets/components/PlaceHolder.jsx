f408d2357f10450da91c2905915fccec82f075c7
Abel Shibabaw <abelshibabaw@heuc.edu.et>
2023-08-20 12:46:46 +0300

    animated placeholder

--- src/assets/components/PlaceHolder.jsx
+++ src/assets/components/PlaceHolder.jsx
@@ -1,8 +1,14 @@
+import { motion } from "framer-motion";
 const PlaceHolder = ({ text }) => {
   return (
-    <div className="flex flex-row items-center px-4 py-2 bg-primary bg-opacity-10 text-secondary font-semibold rounded-full max-md:text-sm">
+    <motion.div
+      initial={{ scaleX: 0, scaleY: 0 }}
+      animate={{ scaleX: 1, scaleY: 1 }}
+      transition={{ duration: 0.6 }}
+      className="flex flex-row items-center px-4 py-2 bg-primary bg-opacity-10 text-secondary font-semibold rounded-full max-md:text-sm"
+    >
       {text.toUpperCase()}
-    </div>
+    </motion.div>
   );
 };
 
